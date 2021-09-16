370af32cf5d3ef9144f1dd004e6b1b6a19da6058
Ouanu <ouanu@foxmail.com>
2021-09-16 16:59:09 +0800

    页面

--- app/src/main/java/com/moment/myapplication/ChatActivity.java
+++ app/src/main/java/com/moment/myapplication/ChatActivity.java
@@ -1,14 +1,14 @@
 package com.moment.myapplication;
 
 import android.content.Context;
-import android.database.Cursor;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +19,11 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mEtChat;
     private ImageView mIvAddPicture;
     private ImageView mIvSend;
-    private ListView mLvChatRecord;
+//    private ListView mLvChatRecord;
 
+
+    private String nowRecord = "";
+    private TextView mTvRecord;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,23 +34,24 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
         mEtChat = findViewById(R.id.et_chat);
         mIvAddPicture = findViewById(R.id.iv_add_picture);
         mIvSend = findViewById(R.id.iv_send);
-        mLvChatRecord = findViewById(R.id.lv_chat_record);
+//        mLvChatRecord = findViewById(R.id.lv_chat_record);
         mIvBack.setOnClickListener(this);
+        mIvSend.setOnClickListener(this);
+        mTvRecord = findViewById(R.id.tv_record);
+
 
         String contactName = getIntent().getStringExtra("contactName");
         long id = getIntent().getLongExtra("id", 0);
         String imageSrc = getIntent().getStringExtra("imageSrc");
 
-
-
-
-
-
+        SharedPreferences sharedPreferences = getSharedPreferences(String.valueOf(id), MODE_PRIVATE);
+        sharedPreferences.getString("all", null);
 
 
         if (id != 0) {
             mTvChatContactName.setText(contactName);
         }
+
 
     }
 
@@ -59,9 +63,11 @@ public class ChatActivity extends AppCompatActivity implements View.OnClickListe
              */
             finish();
         } else if (v.getId() == R.id.iv_send) {
-            /**
-             * 发送信息
-             */
+            String record = mEtChat.getText().toString();
+            nowRecord += "我" + "\n" + record + "\n\n";
+            mTvRecord.setText(nowRecord);
+            mTvRecord.setMovementMethod(ScrollingMovementMethod.getInstance());
+            mEtChat.setText("");
         } else if (v.getId() == R.id.iv_add_picture) {
             /**
              * 调用系统图库，选择图片发送
